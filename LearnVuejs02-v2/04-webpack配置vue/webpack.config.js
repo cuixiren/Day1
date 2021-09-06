99b9f5ede8e6ec02510d114742cef1441ed3d059
cuixr <354932835@qq.com>
2021-09-06 19:12:52 +0800

    0906公司

---
+++ LearnVuejs02-v2/04-webpack配置vue/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader'] //  vue-loader was used without the corresponding plugin. Make sure to include VueLoaderPlugin in your webpack config. 安装>=13的版本
+      },
+      {
+        test: /\.vue$/,
+        use: ['vue-loader'] //  vue-loader was used without the corresponding plugin. Make sure to include VueLoaderPlugin in your webpack config. 安装>=13的版本
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js','.css','.vue'], //解决import时扩展名不能简写的问题
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+}
---
+++ LearnVuejs02-v2/06-webpack配置的分离/build/base.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const uglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'] //  vue-loader was used without the corresponding plugin. Make sure to include VueLoaderPlugin in your webpack config. 安装>=13的版本
+      },
+      {
+        test: /\.vue$/,
+        use: ['vue-loader'] //  vue-loader was used without the corresponding plugin. Make sure to include VueLoaderPlugin in your webpack config. 安装>=13的版本
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.css', '.vue'], //解决import时扩展名不能简写的问题
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  plugins: [
+    new webpack.BannerPlugin('版权所有者--崔喜人'),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+}
